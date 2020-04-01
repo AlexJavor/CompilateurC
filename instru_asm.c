2c9125989de7d440663805bbffa0a82e9ce0bb51
AlexJavor <aleksanjavor@gmail.com>
2020-04-01 11:02:21 +0200

    Assembly instructions generator semi-completed

--- instru_asm.c
+++ instru_asm.c
@@ -1,13 +1,63 @@
-#include "instru_asm.h"
 #include "symbol_table.h"
+#include "instru_asm.h"
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
+#include <stdlib.h>
 
-void create_add_line(char *name_var1, char *name_var2, char *name_var_res)
-{
-    int addr1 = find_symbol_by_name(name_var1);
-    int addr2 = find_symbol_by_name(name_var2);
-    int addr_res = find_symbol_by_name(name_var_res);
-    printf("ADD %d %d %d\n", addr_res, addr1, addr2);
+instruct table_instruct[255];
+int index_asm = 0;
+
+int get_next_asm_line(){
+	return index_asm;
+}
+
+void add_instruct(char cmd[4], int nb_addr, int addr[]){
+	strcpy(table_instruct[index_asm].cmd, cmd);
+	table_instruct[index_asm].nb_addr = nb_addr;
+	for(int i = 0; i< nb_addr; i++){
+		table_instruct[index_asm].addr[i] = addr[i];
+	}
+	index_asm ++;
+}
+
+void print_code(){
+	printf("Generated asm : \n");
+	for(int j = 0; j < index_asm; j++){
+		switch(table_instruct[j].nb_addr){
+			case 0 :
+				printf("%s\n", table_instruct[j].cmd);
+				break;
+			case 1 : 
+				printf("%s %d\n", table_instruct[j].cmd, table_instruct[j].addr[0]);
+				break;
+			case 2 :
+				printf("%s %d %d\n", table_instruct[j].cmd, table_instruct[j].addr[0], table_instruct[j].addr[1]);
+				break;
+			case 3 :
+				printf("%s %d %d %d\n", table_instruct[j].cmd, table_instruct[j].addr[0], table_instruct[j].addr[1], table_instruct[j].addr[2]);
+				break;
+		}
+	} 
+}
+
+
+void print_to_file(){
+	FILE * f = fopen("result.s", "w");
+	for(int j = 0; j < index_asm; j++){
+		switch(table_instruct[j].nb_addr){
+			case 0 :
+				fprintf(f, "%s\n", table_instruct[j].cmd);
+				break;
+			case 1 : 
+				fprintf(f, "%s %d\n", table_instruct[j].cmd, table_instruct[j].addr[0]);
+				break;
+			case 2 :
+				fprintf(f, "%s %d %d\n", table_instruct[j].cmd, table_instruct[j].addr[0], table_instruct[j].addr[1]);
+				break;
+			case 3 :
+				fprintf(f, "%s %d %d %d\n", table_instruct[j].cmd, table_instruct[j].addr[0], table_instruct[j].addr[1], table_instruct[j].addr[2]);
+				break;
+		}
+	} 
+	fclose(f);
 }
--- instru_asm.h
+++ instru_asm.h
@@ -1,9 +1,22 @@
 #ifndef INSTRUASM_H
 #define INSTRUASM_H
 
-#include <stdio.h>
-#include <string.h>
+typedef struct {
+	char cmd[4];
+	int addr[3];
+    int nb_addr;
+} instruct;
 
-void create_add_line(char *name_var1, char *name_var2, char *name_var_res);
+/* Get the next line from the assembly code */
+int get_next_asm_line();
+
+/* Add an instruction to the assembly code */
+void add_instruct(char cmd[4], int nb_addr, int addr[]);
+
+/* Print the assembly code directly to the terminal*/
+void print_asm_code();
+
+/* Write the assembly code into a file */
+void print_asm_to_file();
 
 #endif
